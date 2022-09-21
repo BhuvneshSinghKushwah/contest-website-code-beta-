330856d2f2d6fc0900e1590d398487a56a642ba6
KLIC <lnct@KLICs-iMac.local>
2022-09-21 16:50:46 +0530

    info Page before starting test

---
+++ src/components/Info.js
@@ -0,0 +1,53 @@
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useNavigate } from "react-router-dom"
+import "../cssComponents/Info.css"
+
+const InfoPage = ( {infoState, setinfoState } ) => {
+  const emailRef = useRef()
+  const history = useNavigate();
+  const [loading, setLoading] = useState("false");
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setLoading(true)
+      history.push("/question-page")
+    } catch {
+      console.log("error");
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <>
+     <div className="wrapper">
+        <div className="text-center mt-4 name">
+            LNCT MCQ
+        </div>
+        <form className="p-3 mt-3" onSubmit={handleSubmit}>
+            <div className="form-field d-flex align-items-center" id="fullName">
+                <span className="far fa-user"></span>
+                <input type="text" name="fullName"  placeholder="Enter your full name" deleteKeyCode={null} onChange={(e) => setinfoState(e.target.value)} label= "FullName" value={infoState.fullName} required/>
+            </div>
+            <div className="form-field d-flex align-items-center" id="enrollment">
+                <span className="far fa-user"></span>
+                <input type="text" name="semester"  placeholder="Enter your enrollment" deleteKeyCode={null} onChange={(e) => setinfoState(e.target.value)}  label = "Enrollment" value={infoState.enrollment} required/>
+            </div>
+            <div className="form-field d-flex align-items-center" id="semester">
+                <span className="far fa-user"></span>
+                <input type="text" name="semester"  placeholder="Enter your semester" deleteKeyCode={null} onChange={(e) => setinfoState(e.target.value)}  label = "Semester" value={infoState.semester} required/>
+            </div>
+           <button disabled={loading} className="btn mt-3" type="submit">
+                Start Test
+           </button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default InfoPage
